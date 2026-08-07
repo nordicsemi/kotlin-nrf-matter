968f65b5f5ec22b8a7df16e49b85990f7a673074
hiar <aryal.himali@gmail.com>
2026-08-07 13:32:04 +0200

    Binding state handler

---
+++ composeApp/src/commonMain/kotlin/no/nordicsemi/nrf/matter/binding/BindingStateHandler.kt
@@ -0,0 +1,123 @@
+package no.nordicsemi.nrf.matter.binding
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.WarningAmber
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import kotlinx.collections.immutable.PersistentList
+import multiplatform.network.cmptoast.ToastDuration
+import multiplatform.network.cmptoast.ToastGravity
+import multiplatform.network.cmptoast.showToast
+import no.nordicsemi.nrf.matter.domain.BindingState
+import no.nordicsemi.nrf.matter.domain.UiState
+import no.nordicsemi.nrf.matter.logger.NordicLogger
+import no.nordicsemi.nrf.matter.theme.NordicSun
+import no.nordicsemi.nrf.matter.ui.AlertDialogView
+
+/*
+ * Copyright (c) 2025, Nordic Semiconductor
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list
+ * of conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without specific prior
+ * written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+@Composable
+internal fun BindingStateHandler(
+    bindingUiState: BindingUiState,
+    bindingLogs: PersistentList<String>,
+    onUpdateBindingState: (BindingState) -> Unit
+) {
+    when (bindingUiState.bindingState) {
+        is UiState.Error -> {
+            AlertDialogView(
+                onDismiss = {
+                    // Change state to idle.
+                    onUpdateBindingState(UiState.Idle())
+                },
+                onConfirm = {
+                    // Retry binding. Set state to loading and call the binding function again.
+                },
+                title = "Binding Failed.",
+                message = "Unable to bind the device, please try again.",
+                confirmText = "Retry"
+            )
+        }
+
+        is UiState.Idle -> {
+            // Do nothing, show the normal UI.
+        }
+
+        is UiState.Loading -> {
+            BindingLoaderDialog(bindingLogs) {
+                // Text Content
+                Text(
+                    text = "Binding...",
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Bold,
+                    color = if (isSystemInDarkTheme()) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.inverseOnSurface
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.WarningAmber,
+                        contentDescription = null,
+                        tint = NordicSun
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = "Binding in progress, it might take few seconds. Please don't close the app.",
+                        color = if (isSystemInDarkTheme()) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.inverseOnSurface
+                    )
+                }
+            }
+        }
+
+        is UiState.Success -> {
+            NordicLogger.info("Binding Success", tag = "Bindings")
+            showToast(
+                message = "Binding completed successfully!",
+                duration = ToastDuration.Long,
+                gravity = ToastGravity.Center
+            )
+        }
+    }
+}
